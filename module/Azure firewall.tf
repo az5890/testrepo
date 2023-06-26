35270e3d390fc02778200a65ac6b91eca5a44eb6
az5890 <az5890@hotmail.com>
2023-06-26 12:15:14 +0530

    A

--- module/Azure firewall.tf
+++ module/Azure firewall.tf
@@ -15,7 +15,7 @@ resource "azurerm_virtual_network" "example" {
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "my-subnet"
+  name                 = "AzureBastionSubnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.0.0/24"]
