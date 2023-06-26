338b2c5faaecdbc429432aef2f308874b036f764
az5890 <az5890@hotmail.com>
2023-06-26 13:54:50 +0530

    X

--- module/Azure firewall.tf
+++ module/Azure firewall.tf
@@ -7,19 +7,19 @@
 #   location = "East US"
 # }
 
-# resource "azurerm_virtual_network" "example" {
-#   name                = "my-virtual-network"
-#   address_space       = ["10.0.0.0/16"]
-#   location            = azurerm_resource_group.example.location
-#   resource_group_name = azurerm_resource_group.example.name
-# }
+resource "azurerm_virtual_network" "example" {
+  name                = "my-virtual-network"
+  address_space       = ["10.0.0.0/18"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
 
-# resource "azurerm_subnet" "example" {
-#   name                 = "AzureBastionSubnet"
-#   resource_group_name  = azurerm_resource_group.example.name
-#   virtual_network_name = azurerm_virtual_network.example.name
-#   address_prefixes     = ["10.0.0.0/24"]
-# }
+resource "azurerm_subnet" "example1" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
 
 resource "azurerm_public_ip" "example" {
   name                = "my-public-ip"
@@ -38,7 +38,7 @@ resource "azurerm_bastion_host" "example" {
 
 ip_configuration {
     name      = "ipconfig1"
-    subnet_id = azurerm_subnet.example
+    subnet_id = azurerm_subnet.example1.id
     public_ip_address_id = azurerm_public_ip.example.id
   }
 }
