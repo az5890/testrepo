ac509be85b4e84b7922816ef03ce99373450c1ab
az5890 <az5890@hotmail.com>
2023-06-26 16:14:19 +0530

    re

--- module/Recoveryservicevault.tf
+++ module/Recoveryservicevault.tf
@@ -6,28 +6,43 @@ resource "azurerm_resource_group" "example" {
 
 # Create the Recovery Services Vault
 resource "azurerm_recovery_services_vault" "example" {
-  name                = "recovery-vault"
+  name                = "tfex-recovery-vault"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   sku                 = "Standard"  # Replace with desired SKU
 }
 
 
-# Create the backup policy
-resource "azurerm_recovery_services_protection_policy" "example" {
-  name                = "backup-policy"
+resource "azurerm_backup_policy_vm" "example" {
+  name                = "tfex-recovery-vault-policy"
   resource_group_name = azurerm_resource_group.example.name
- # vault_name          = azurerm_recovery_services_vault.example.name
+  recovery_vault_name = azurerm_recovery_services_vault.example.name
+
+  timezone = "UTC"
 
   backup {
-    frequency        = "Daily"
+    frequency = "Daily"
     retention_days   = 30
     start_time       = "02:00"
     time_zone        = "UTC"
   }
-}
 
-# Output the vault details
-output "vault_id" {
-  value = azurerm_recovery_services_vault.example.id
-}
+
+# # Create the backup policy
+# resource "azurerm_recovery_services_protection_policy" "example" {
+#   name                = "backup-policy"
+#   resource_group_name = azurerm_resource_group.example.name
+#  # vault_name          = azurerm_recovery_services_vault.example.name
+
+#   backup {
+#     frequency        = "Daily"
+#     retention_days   = 30
+#     start_time       = "02:00"
+#     time_zone        = "UTC"
+#   }
+# }
+
+# # Output the vault details
+# output "vault_id" {
+#   value = azurerm_recovery_services_vault.example.id
+# }
