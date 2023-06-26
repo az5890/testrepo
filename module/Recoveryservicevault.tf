ff0282efc218fe8f02539380040f49b4116f2f73
az5890 <az5890@hotmail.com>
2023-06-26 15:49:24 +0530

    e

--- module/Recoveryservicevault.tf
+++ module/Recoveryservicevault.tf
@@ -1,6 +1,6 @@
 # Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = "recovery-vault-rg"
+  name     = "recovery-vault-rg1"
   location = "East US"
 }
 
@@ -11,6 +11,20 @@ resource "azurerm_recovery_services_vault" "example" {
   location            = azurerm_resource_group.example.location
   sku                 = "Standard"  # Replace with desired SKU
 }
+
+
+# Create the backup policy
+resource "azurerm_recovery_services_protection_policy_vm" "example" {
+  name                = "backup-policy"
+  resource_group_name = azurerm_resource_group.example.name
+  vault_name          = azurerm_recovery_services_vault.example.name
+
+  backup {
+    frequency        = "Daily"
+    retention_days   = 30
+    start_time       = "02:00"
+    time_zone        = "UTC"
+  }
 
 # Output the vault details
 output "vault_id" {
