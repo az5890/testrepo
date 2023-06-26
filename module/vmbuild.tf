7d72c8ba305edd735fc536190317a5a71461500a
az5890 <az5890@hotmail.com>
2023-06-26 18:50:03 +0530

    commit

--- module/vmbuild.tf
+++ module/vmbuild.tf
@@ -22,12 +22,12 @@ resource "azurerm_resource_group" "example" {
 }
 
 # Define the virtual network
-# resource "azurerm_virtual_network" "example" {
-#   name                = var.vnet
-#   address_space       = ["10.0.0.0/16"]
-#   location            = azurerm_resource_group.example.location
-#   resource_group_name = azurerm_resource_group.example.name
-# }
+resource "azurerm_virtual_network" "example" {
+  name                = var.vnet
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
 
 # Define the subnet
 resource "azurerm_subnet" "example" {
