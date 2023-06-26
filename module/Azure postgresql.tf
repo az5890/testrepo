085bace15b923586162772e2b07dd7bad668d5ab
az5890 <az5890@hotmail.com>
2023-06-26 17:29:43 +0530

    a

--- module/Azure postgresql.tf
+++ module/Azure postgresql.tf
@@ -1,6 +1,6 @@
 #Azure DB
 resource "azurerm_postgresql_server" "example" {
-  name                = "my-postgres-server"
+  name                = "my-postgres-server004"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   sku_name            = "GP_Gen5_8"
