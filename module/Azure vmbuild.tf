42923a88b9eb8fdf4d33eb3f2b8d4f39513a79f6
az5890 <az5890@hotmail.com>
2023-06-26 12:45:57 +0530

    commit

--- module/Azure vmbuild.tf
+++ module/Azure vmbuild.tf
@@ -17,7 +17,7 @@
 
 # Define the resource group
 resource "azurerm_resource_group" "example" {
-  name     = "RG"
+  name     = "RG001"
   location = "East US"
 }
 
@@ -31,7 +31,7 @@ resource "azurerm_virtual_network" "example" {
 
 # Define the subnet
 resource "azurerm_subnet" "example" {
-  name                 = "my-subnet"
+  name                 = "AzureBastionSubnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.1.0/24"]
