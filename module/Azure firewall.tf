1f947dff8e27d949ed030f54ccfefae0e1659b45
az5890 <az5890@hotmail.com>
2023-06-26 10:59:17 +0530

    commmit

--- module/Azure firewall.tf
+++ module/Azure firewall.tf
@@ -1,6 +1,7 @@
+
 resource "azurerm_resource_group" "example" {
   name     = "my-resource-group"
-  location = "East US"
+  location = "westus2"
 }
 
 resource "azurerm_virtual_network" "example" {
@@ -24,45 +25,63 @@ resource "azurerm_public_ip" "example" {
   allocation_method   = "Static"
 }
 
-resource "azurerm_firewall" "example" {
-  name                = "my-firewall"
+resource "azurerm_bastion_host" "example" {
+  name                = "my-bastion"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  sku_tier            = "Standard"
+  virtual_network_subnet_ids = [azurerm_subnet.example.id]
+  public_ip_address_id       = azurerm_public_ip.example.id
 }
 
-resource "azurerm_firewall_application_rule_collection" "example" {
-  name                = "my-application-rule-collection"
+output "bastion_hostname" {
+  value       = azurerm_bastion_host.example.dns_name
+  description = "Hostname of the Azure Bastion"
+}
+
+output "bastion_public_ip" {
+  value       = azurerm_bastion_host.example.ip_configuration.0.public_ip_address
+  description = "Public IP address of the Azure Bastion"
+}
+resource "azurerm_resource_group" "example" {
+  name     = "my-resource-group"
+  location = "westus2"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "my-virtual-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  firewall_name       = azurerm_firewall.example.name
-
-  rule {
-    name                  = "allow-http-https"
-    description           = "Allow HTTP and HTTPS traffic from any source to any destination"
-    protocol              = "Http"
-    source_addresses      = ["*"]
-    destination_addresses = ["*"]
-    target_fqdns          = []
-    fqdn_tags             = []
-  }
 }
 
-resource "azurerm_firewall_network_rule_collection" "example" {
-  name                = "my-network-rule-collection"
+resource "azurerm_subnet" "example" {
+  name                 = "my-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "my-public-ip"
+  location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  firewall_name       = azurerm_firewall.example.name
-
-  rule {
-    name                  = "allow-http-https"
-    description           = "Allow HTTP and HTTPS traffic from any source to any destination"
-    protocols             = ["TCP"]
-    source_addresses      = ["*"]
-    destination_addresses = ["*"]
-    destination_ports     = ["80", "443"]
-  }
+  allocation_method   = "Static"
 }
 
-output "firewall_private_ip" {
-  value       = azurerm_firewall.example.ip_configuration[0].private_ip_address
-  description = "Private IP address of the Azure Firewall"
+resource "azurerm_bastion_host" "example" {
+  name                = "my-bastion"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  virtual_network_subnet_ids = [azurerm_subnet.example.id]
+  public_ip_address_id       = azurerm_public_ip.example.id
+}
+
+output "bastion_hostname" {
+  value       = azurerm_bastion_host.example.dns_name
+  description = "Hostname of the Azure Bastion"
+}
+
+output "bastion_public_ip" {
+  value       = azurerm_bastion_host.example.ip_configuration.0.public_ip_address
+  description = "Public IP address of the Azure Bastion"
 }
