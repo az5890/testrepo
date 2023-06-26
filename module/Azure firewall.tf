7f6f5d5ecf9a7876c4083b66af90ba96f458aa1b
az5890 <az5890@hotmail.com>
2023-06-26 10:54:22 +0530

    commit

--- module/Azure firewall.tf
+++ module/Azure firewall.tf
@@ -30,41 +30,39 @@ resource "azurerm_firewall" "example" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
    sku_tier            = "Standard" 
-  ip_configurations   = [{
-    name                          = "my-firewall-ip-config"
-    subnet_id                     = azurerm_subnet.example.id
-    public_ip_address_id          = azurerm_public_ip.example.id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = "10.0.0.4"  # Update with the desired private IP address
-  }]
 }
-
 resource "azurerm_firewall_application_rule_collection" "example" {
   name                = "my-application-rule-collection"
   resource_group_name = azurerm_resource_group.example.name
   firewall_name       = azurerm_firewall.example.name
 
   rule {
-    name                     = "allow-http-https"
-    description              = "Allow HTTP and HTTPS traffic from any source to any destination"
-    protocol_type            = "Http"
-    protocol                 = "TCP"
-    source_addresses         = ["*"]
-    destination_addresses    = ["*"]
-    destination_ports        = ["80", "443"]
-    target_fqdns             = []
-    fqdn_tags                = []
-    azure_active_directory   = []
-    source_ip_groups         = []
-    application_rule_protocols = []
+    name                  = "allow-http-https"
+    description           = "Allow HTTP and HTTPS traffic from any source to any destination"
+    protocol              = "Http"
+    source_addresses      = ["*"]
+    destination_addresses = ["*"]
+    target_fqdns          = []
+    fqdn_tags             = []
+  }
+}
+
+resource "azurerm_firewall_network_rule_collection" "example" {
+  name                = "my-network-rule-collection"
+  resource_group_name = azurerm_resource_group.example.name
+  firewall_name       = azurerm_firewall.example.name
+
+  rule {
+    name                  = "allow-http-https"
+    description           = "Allow HTTP and HTTPS traffic from any source to any destination"
+    protocols             = ["TCP"]
+    source_addresses      = ["*"]
+    destination_addresses = ["*"]
+    destination_ports     = ["80", "443"]
   }
 }
 
 output "firewall_private_ip" {
-  value       = azurerm_firewall.example.ip_configurations[0].private_ip_address
+  value       = azurerm_firewall.example.ip_configuration[0].private_ip_address
   description = "Private IP address of the Azure Firewall"
 }
-
-
-
-
