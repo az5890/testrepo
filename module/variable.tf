48feeddd0f48c648ce5a6ffe8c5535c5f282096d
az5890 <az5890@hotmail.com>
2023-06-26 16:56:05 +0530

    q

--- module/variable.tf
+++ module/variable.tf
@@ -24,7 +24,7 @@ variable "VM00" {
     default = "Test1"
     description = "value" 
 }
-variable "dbservername" {  
+variable "db_servername00" {  
     default = "Test1"
     description = "value" 
 }
