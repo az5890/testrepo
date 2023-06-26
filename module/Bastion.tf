9ccd3aa4319578ebf5aa28c90df6920d4bae9db8
az5890 <az5890@hotmail.com>
2023-06-26 18:39:09 +0530

    a

--- module/Bastion.tf
+++ module/Bastion.tf
@@ -15,7 +15,7 @@ resource "azurerm_virtual_network" "example" {
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = Azure_Bastion_Subnet
+  name                 = AzureBastionSubnet
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.0.0/24"]
