2ce94cfa6848485cd51476e2485539d79f6ba0fd
az5890 <az5890@hotmail.com>
2023-06-26 11:32:27 +0530

    Z

--- module/Azure firewall.tf
+++ module/Azure firewall.tf
@@ -32,7 +32,7 @@ resource "azurerm_bastion_host" "example" {
   name                = "my-bastion"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
- # subnet_ids          = azurerm_subnet.example.id
+  subnet_id          = azurerm_subnet.example.id
  # public_ip_address_id = azurerm_public_ip.example.id
 }
 
@@ -42,6 +42,6 @@ output "bastion_hostname" {
 }
 
 output "bastion_public_ip" {
-  value       = azurerm_bastion_host.example.ip_configuration[0].ip_address
+  value       = azurerm_bastion_host.example.ip_configuration[0].public_ip_address
   description = "Public IP address of the Azure Bastion"
 }
